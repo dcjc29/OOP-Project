b87c11e8e88890095c0ba8ec32d2fb069343b16b
dcjc29 <durekshawasala@gmail.com>
2019-05-19 20:11:07 +0530

    Applied Singleton Pattern To DB Connection

--- src/util/DBConnectionUtil.java
+++ src/util/DBConnectionUtil.java
@@ -5,34 +5,27 @@ import java.sql.*;
 import java.util.Properties;
 
 
-public class DBConnectionUtil{
+public class DBConnectionUtil extends CommonUtil{
 	static InputStream in = null;
-	String msg = null;
+	static String msg = null;
 	private static Connection con;
 	public static final Properties props = new Properties();
 	
 
-	public  Connection getDBConnection(){
+	private DBConnectionUtil(){
+		
+	}
+	public static Connection getDBConnection(){
 		
 	try
 	{
-		//File inFile = new File("/BidWars/src/util/config.properties");
-		String propname = "config.properties";
-		in=getClass().getResourceAsStream(propname);
-		if(in!=null) {
-		props.load(in);
+		if (con == null || con.isClosed()) {
+
+			Class.forName(properties.getProperty(CommonConstants.DRIVER_NAME));
+			con = DriverManager.getConnection(properties.getProperty(CommonConstants.URL),
+					properties.getProperty(CommonConstants.USERNAME), properties.getProperty(CommonConstants.PASSWORD));
 		}
-
-		String driver = props.getProperty("driverName");
-		String url = props.getProperty("url");
-		String username = props.getProperty("username");
-		String password = props.getProperty("password");
 		
-
-		if (driver != null) {
-			Class.forName(driver);
-			con = DriverManager.getConnection(url, username, password);
-	}
 	
 	}catch(Exception e) {
 		 msg=e.toString();
